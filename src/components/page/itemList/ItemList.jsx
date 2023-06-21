487554d9b79788c043ffcbaa7448bf719bccdf09
jorgeduje <jorgeduje4@gmail.com>
2023-06-21 11:55:49 -0300

    se trabajo con useEffect y counter

--- src/components/page/itemList/ItemList.jsx
+++ src/components/page/itemList/ItemList.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react"
 
-const ItemList = (  ) => {
+const ItemList = ( {edad} ) => {
 
+  const [items, setItems] = useState([])
+
+  // Se ejecuta el efecto solo una vez, en el montaje del componente
+
+  useEffect( ()=>{
+    // ACA ES DONDE VA TODO CODIGO QUE NO QUIERO QUE SE LEA NUEVAMENTE
+    console.log("pedimos datos a la API")
+  } , [] )
+
+  // Se ejecuta el efecto una vez, en el montaje del componente pero tambien
+  // cada vez que escucha un cambio en sus dependencias!
+  // useEffect( ()=>{
+  //   // ACA ES DONDE VA TODO CODIGO QUE NO QUIERO QUE SE LEA NUEVAMENTE
+  //   console.log("pedimos datos a la API")
+  // } , [ items ] )
+
+  console.log("me ejecute primero")
 
   return (
     <div>
       <h1>Aca van los productos</h1>
+      <h2>la edad es { edad }</h2>
+      <button onClick={()=>setItems([...items, {}])}>Traer productos</button>
     </div>
   )
 }
