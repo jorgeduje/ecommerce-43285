9ec4f162320d08fe788dd3e604d841b003220666
jorgeduje <jorgeduje4@gmail.com>
2023-06-26 11:46:31 -0300

    se trabajo con promesas y mapeo de arreglos

--- src/components/page/itemList/ItemList.jsx
+++ src/components/page/itemList/ItemList.jsx
@@ -1,16 +1,26 @@
-import { useState, useEffect } from "react";
+import ProductCard from "../../common/productCard/ProductCard";
 
-const ItemList = () => {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    // ACA ES DONDE VA TODO CODIGO QUE NO QUIERO QUE SE LEA NUEVAMENTE
-    console.log("pedimos datos a la API");
-  }, []);
+const ItemList = ({ items }) => {
+  console.log("llego al presentacional los items: ", items);
+  // console.log({items})
+  // console.log(items)
 
   return (
     <div>
       <h1>Aca van los productos</h1>
+
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+        }}
+      >
+        {items.map((elemento) => {
+          return <ProductCard key={elemento.id} elemento={elemento} />;
+        })}
+      </div>
     </div>
   );
 };
