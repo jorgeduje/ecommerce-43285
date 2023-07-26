7dbb7e2c66355fec61c619c09f148c6887c9e82c
jorgeduje <jorgeduje4@gmail.com>
2023-07-26 10:14:36 -0300

    se agregan variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFD8bsufwBhavP17W8Eg8uoguJhhU8su4",
-  authDomain: "comision-43285.firebaseapp.com",
-  projectId: "comision-43285",
-  storageBucket: "comision-43285.appspot.com",
-  messagingSenderId: "698775587210",
-  appId: "1:698775587210:web:0e58297fd4d37125832073"
+  apiKey: import.meta.env.VITE_APIKEY ,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId:import.meta.env.VITE_MESSAGING ,
+  appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
