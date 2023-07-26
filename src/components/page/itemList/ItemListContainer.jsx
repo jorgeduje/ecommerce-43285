725896a4892f10850f70435da8e26019f4468b4b
jorgeduje <jorgeduje4@gmail.com>
2023-07-26 11:52:45 -0300

    auth con firebase

--- src/components/page/itemList/ItemListContainer.jsx
+++ src/components/page/itemList/ItemListContainer.jsx
@@ -2,39 +2,44 @@ import { useState, useEffect } from "react";
 
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
-import { db } from "../../../firebaseConfig";
+import { db, ingresarConGoogle } from "../../../firebaseConfig";
 import { getDocs, collection, query, where } from "firebase/firestore";
-
-
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
 
   const { categoryName } = useParams();
 
+  const ingresarGoogle = async()=>{
+    let res = await ingresarConGoogle()
+    console.log(res)
+  }
+
   useEffect(() => {
     let productsCollection = collection(db, "products");
     let consulta;
-    if(categoryName){
-      consulta = query( productsCollection, where("category", "==", categoryName))
-    }else{
-      consulta = productsCollection
+    if (categoryName) {
+      consulta = query(
+        productsCollection,
+        where("category", "==", categoryName)
+      );
+    } else {
+      consulta = productsCollection;
     }
 
-    getDocs(consulta).then((res)=>{
-      let productos = res.docs.map( doc => {
-        return {...doc.data(), id: doc.id}
-      } )
-      
-      setItems(productos)
-    })
-  
+    getDocs(consulta).then((res) => {
+      let productos = res.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+
+      setItems(productos);
+    });
   }, [categoryName]);
 
   return (
     <>
+      <button onClick={ingresarGoogle}>Google</button>
       <ItemList items={items} />
-   
     </>
   );
 };
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,56 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY ,
+  apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId:import.meta.env.VITE_PROJECTID,
-  storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
-  messagingSenderId:import.meta.env.VITE_MESSAGING ,
-  appId: import.meta.env.VITE_APPID
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
+const auth = getAuth(app);
+
+// ESTO VA EN EL ONSUBMIT DEL FORMULARIO DE LOGIN
+export const login = async ({ email, password }) => {
+  let res = await signInWithEmailAndPassword(auth, email, password);
+  return res;
+};
+
+// ESTO VA EN EL ONSUBMIT DEL FORMULARIO DE registro
+export const register = async({email,password}) => {
+  let res = await createUserWithEmailAndPassword(auth, email, password)
+  return res
+};
+
+let proveedorDeGoogle = new GoogleAuthProvider()
+
+export const ingresarConGoogle = async ()=>{
+  let res = await signInWithPopup(auth,proveedorDeGoogle )
+  return res  
+}
+
+// {/* <button onClick={ingresarConGoogle}>Google</button> */}
+
+
+// input -- email
+//  input  --- passowrd
+
+//  onsubmit()=>{
+//    let resultado = register({email: "pepee", password: 1234})
+// if(resultado.accessToken){
+  // login()
+// }else{
+
+// }
+//  }
